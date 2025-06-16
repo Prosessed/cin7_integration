cfbcca3d6a72b30400ebd18176315d0ee6cd50a7
codewithjaspreet <jaspreetsodhi02@gmail.com>
2025-06-16 11:24:25 +0000

    fix : optimised sync functions

--- cin7_integration/cin7_integration/doctype/cin7_settings/cin7_settings.py
+++ cin7_integration/cin7_integration/doctype/cin7_settings/cin7_settings.py
@@ -653,7 +653,7 @@ def sync_stock():
 @frappe.whitelist()
 def sync_cin7_sales_orders_background():
     frappe.enqueue(
-        "cin7_integration.cin7_integration.cin7_settings.sync_sales_orders",
+        "cin7_integration.cin7_integration.doctype.cin7_settings.cin7_settings.sync_sales_orders",
         queue="long",
         timeout=1800,
         job_name="Sync CIN7 Sales Orders",
