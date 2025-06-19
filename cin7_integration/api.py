397a98cf079c461cfa6bd9a859c1081861a2d7b2
codewithjaspreet <jaspreetsodhi02@gmail.com>
2025-06-19 10:18:23 +0000

    fix : order date removed default

--- cin7_integration/api.py
+++ cin7_integration/api.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 import frappe
 import requests
 import json
@@ -7,20 +6,25 @@ from frappe.utils import now_datetime, add_days
 from cin7_integration.cin7_integration.doctype.cin7_integration_log.cin7_integration_log import log_cin7
 
 
-def create_sales_order_on_cin7(doc, method):
-    # 👉 Skip CIN7 sync if Sales Order already has CIN7 Sale ID
-    if doc.get("custom_cin7_sale_id"):
-        frappe.msgprint("CIN7 Sale ID already present, skipping CIN7 sync.")
-        return
 
-    # Proceed with normal CIN7 sync
+def create_sales_order_on_cin7(doc, method):
+    # Step 1: Initiate order in CIN7
     initiate_sales_order_on_cin7(doc)
+
+    # Step 2: Add line items to CIN7 order
     place_order_lines_on_cin7(doc)
 
 def initiate_sales_order_on_cin7(doc):
     if doc.get("custom_is_synced"):
         frappe.msgprint("Sales Order is already synced with CIN7.")
         return
+
+    if doc.get("custom_cin7_sale_id") is None:
+        frappe.msgprint('Sales Order Already there on CIN7')
+        doc.workflow_state = 'Invoiced'
+        frappe.db.commit()
+        return
+
 
     cin7_settings = frappe.get_single("CIN7 Settings")
     api_url = "https://inventory.dearsystems.com/ExternalApi/v2/sale"
@@ -198,17 +202,7 @@ def create_erpnext_sales_order_from_cin7(sale_data: dict) -> str | None:
         doc = frappe.new_doc("Sales Order")
         doc.naming_series = "SO-"
         doc.customer = customer_name
-
-
-        order_date_str = sale_data.get("OrderDate")
-
-        if not order_date_str or not isinstance(order_date_str, str):
-            frappe.throw(f"Invalid or missing OrderDate: {order_date_str}")
-
-        order_date = datetime.fromisoformat(order_date_str).date()
-        doc.transaction_date = order_date
-        doc.delivery_date = add_days(order_date, 1)
-
+        doc.transaction_date = sale_data.get("OrderDate")
         doc.delivery_date = add_days(doc.transaction_date, 1)
         doc.po_no = sale_data.get("SaleOrderNumber")
         doc.custom_cin7_sale_id = sale_id
@@ -237,3 +231,5 @@ def create_erpnext_sales_order_from_cin7(sale_data: dict) -> str | None:
             message=f"Error creating Sales Order for {sale_data.get('SaleOrderNumber')}: {str(e)}"
         )
         return None
+
+
