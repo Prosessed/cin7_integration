6c27bcd1f133ac00ec2fdf9a00eac5143fabe99f
codewithjaspreet <jaspreetsodhi02@gmail.com>
2025-06-19 09:15:46 +0000

    fix : order date and sync logic

--- cin7_integration/api.py
+++ cin7_integration/api.py
@@ -7,25 +7,20 @@ from frappe.utils import now_datetime, add_days
 from cin7_integration.cin7_integration.doctype.cin7_integration_log.cin7_integration_log import log_cin7
 
 
-
 def create_sales_order_on_cin7(doc, method):
-    # Step 1: Initiate order in CIN7
-    initiate_sales_order_on_cin7(doc)
+    # 👉 Skip CIN7 sync if Sales Order already has CIN7 Sale ID
+    if doc.get("custom_cin7_sale_id"):
+        frappe.msgprint("CIN7 Sale ID already present, skipping CIN7 sync.")
+        return
 
-    # Step 2: Add line items to CIN7 order
+    # Proceed with normal CIN7 sync
+    initiate_sales_order_on_cin7(doc)
     place_order_lines_on_cin7(doc)
 
 def initiate_sales_order_on_cin7(doc):
     if doc.get("custom_is_synced"):
         frappe.msgprint("Sales Order is already synced with CIN7.")
         return
-
-    if doc.get("custom_cin7_sale_id") is None:
-        frappe.msgprint('Sales Order Already there on CIN7')
-        doc.workflow_state = 'Invoiced'
-        frappe.db.commit()
-        return
-
 
     cin7_settings = frappe.get_single("CIN7 Settings")
     api_url = "https://inventory.dearsystems.com/ExternalApi/v2/sale"
